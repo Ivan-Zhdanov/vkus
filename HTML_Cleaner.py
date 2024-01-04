d7a736f0fb60276cf4a467fd3fbb0239d00179ff
Administrator <ivan.zh.moscow@mail.ru>
2024-01-04 16:24:04 +0300

    последние с новым файлом

--- HTML_Cleaner.py
+++ HTML_Cleaner.py
@@ -3,7 +3,7 @@ def html_cleaner(html):
     t1 = html.replace("«»»", "").replace('"""', "")
     t2 = t1.replace("&lt;/li&gt;", "")
     t3 = t2.replace("&lt;li&gt;", "")
-    t4 = t3.replace ("Перепиши с дополнением оставляя html теги:", "")
+    t4 = t3.replace("Перепиши с дополнением оставляя html теги:", "")
     t5 = t4.replace("»»»", "")
     t6 = t5.replace("«»»", "")
     cl_html = t6.strip('"')
